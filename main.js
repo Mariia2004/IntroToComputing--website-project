4dbc1fb050fd307d3690c9fe4a038e1fe1c4b4f0
Mariia2004 <144547379+Mariia2004@users.noreply.github.com>
2024-01-14 10:40:48 +0800

    Website-project
    Intro to Computing website peoject

---
+++ main.js
@@ -0,0 +1,37 @@
+/*var navigation = document.getElementById("navlinks");
+function showMenu(){
+   navigation.style.right="0";
+}
+   function hideMenu(){
+      navigation.style.right="-200px"
+   }
+}*/
+
+/*function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }*/
+
+
+/*let currentIndex = 0;
+
+  function showSlide(index) {
+    const carouselInner = document.querySelector('.carousel-inner');
+    const slideWidth = document.querySelector('.carousel-item').clientWidth;
+    carouselInner.style.transform = `translateX(${-index * slideWidth}px)`;
+  }
+
+  function prevSlide() {
+    currentIndex = (currentIndex - 1 + 3) % 3;
+    showSlide(currentIndex);
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % 3;
+    showSlide(currentIndex);
+  }
+  }*/
